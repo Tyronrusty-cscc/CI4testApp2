dbc2e08711f11431c7ba461cbc67ace9032b5a02
trusty <trusty@cscc.edu>
2024-09-18 15:02:21 -0400

    FIXED the validation

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -7,7 +7,7 @@ use CodeIgniter\Validation\StrictRules\CreditCardRules;
 use CodeIgniter\Validation\StrictRules\FileRules;
 use CodeIgniter\Validation\StrictRules\FormatRules;
 use CodeIgniter\Validation\StrictRules\Rules;
-use  App\Validation\userRules;
+use  App\Validation\UserRules;
 
 
 class Validation extends BaseConfig
